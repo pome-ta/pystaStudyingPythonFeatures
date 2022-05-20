01efacf1d31c8c28e5b327cd7dccde7eac81ecff
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-21 00:03:15 +0900

    up

---
+++ stringManipulation/sandbox02.py
@@ -0,0 +1,16 @@
+HEX_LETTERS = '0123456789abcdef'
+
+i = HEX_LETTERS.find('1'.lower())
+
+'''
+i = None
+try:
+  i = HEX_LETTERS.index('g')
+except ValueError:
+  pass
+'''
+
+
+
+print(i)
+
---
+++ stringManipulation/sandbox03.py
@@ -0,0 +1,9 @@
+i = '\u3042'
+s = i.encode('UTF-8')
+
+a = 'あ'
+ab = a.encode('utf-8')
+
+HEX_LETTERS = '0123456789abcdef'
+s_num = '3042'
+
