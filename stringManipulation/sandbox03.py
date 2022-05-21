78ef07e6f2aabb6c3e634336e02cfc68a8cc8eea
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-21 18:51:04 +0900

    u16

--- stringManipulation/sandbox03.py
+++ stringManipulation/sandbox03.py
@@ -1,9 +1,22 @@
-i = '\u3042'
-s = i.encode('UTF-8')
+ii = '\u3042'
+ss = ii.encode('UTF-8')
 
 a = 'あ'
-ab = a.encode('utf-8')
+#ab = a.encode('utf-8')
+ab = a.encode('utf_16_be')
+intab = int.from_bytes(ab, byteorder='big')
+
 
 HEX_LETTERS = '0123456789abcdef'
 s_num = '3042'
 
+u = 0
+for s in s_num:
+  i = HEX_LETTERS.find(s)
+  u = u * 16 | i
+
+print(chr(u))
+
+
+emoji = '🤔'
+bemoji = emoji.encode('utf-8')
